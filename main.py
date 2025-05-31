de31c7236ab3a2b6f6338d6687a388b3923bd96d
jesurodriguezg <166758920+jesurodriguezg@users.noreply.github.com>
2025-05-31 12:18:45 -0500

    suma y multiplicacion

---
+++ main.py
@@ -0,0 +1,15 @@
+def suma(n1,n2):
+    return n1+n2
+
+def multiplicacion(n1,n2):
+    return n1*n2
+
+def main():
+   n1 = 2
+   n2 = 3
+
+   print(f"La suma es: ",suma(n1,n2))
+   print(f"La multiplicación es: ",multiplicacion(n1,n2))
+
+if _name_ == "_main_":
+  main()
